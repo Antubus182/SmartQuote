babd578413383302c7c2cfe8355614f3cb27a062
Niels ten Thije <niels.ten.thije@nanobay.com>
2017-12-14 16:12:02 +0100

    Wrote the replacement function

--- SmartQuoteReplace.php
+++ SmartQuoteReplace.php
@@ -1,4 +1,12 @@
 <?php
-echo "replacer\n"
+
+function SmartQuoteReplace($inputString){
+    $DumbQuote="e2809d"; //this is the hexadecimal representation of the apple "smart" quote
+    $HexString=bin2hex($inputString);
+    $GoodString=str_replace($DumbQuote,"22",$HexString); //22 is the hexadecimal representation of a regular double qoute
+    $ReplacedString=hex2bin($GoodString);
+
+    return $ReplacedString;
+}
 
 ?>
